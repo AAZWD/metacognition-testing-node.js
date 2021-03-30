eb569238707e3f1ab3f0e6570ac1ce5fff6ccbb8
AAZWD <azumrawi@gmail.com>
2021-03-30 15:08:19 -0700

    3/30

--- public/user/js/script.js
+++ public/user/js/script.js
@@ -1,12 +1,14 @@
 window.onload = function () {
-    //filter patient directory
-
+    //filter by name
     let input = document.getElementById('nameSearch');
-    let tRow = document.querySelectorAll('tbody tr');
+    let tRow = document.querySelectorAll('tbody .info');
     input.onkeyup = function (event) {
         if (input.value) {
             tRow.forEach(row => {
-                if (!row.textContent.includes(input.value))
+                console.log(row)
+                let name = row.querySelector('.name');
+                console.log(name)
+                if (!name.textContent.includes(input.value))
                     row.classList.add('hide');
             });
         }else{
@@ -16,5 +18,24 @@ window.onload = function () {
         }
         
     }
+
+
+       //filter by id
+       let input2 = document.getElementById('idSearch');
+       let tRow2 = document.querySelectorAll('tbody .info');
+       input2.onkeyup = function (event) {
+           if (input2.value) {
+               tRow2.forEach(row => {
+                   let id = row.querySelector('.id');
+                   if (!id.textContent.includes(input2.value))
+                       row.classList.add('hide');
+               });
+           }else{
+               tRow2.forEach(row => {
+                       row.classList.remove('hide');
+               });
+           }
+           
+       }
     
 };
