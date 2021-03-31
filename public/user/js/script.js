0abb92f9eeb5a32e4254a244d227a66d07114c0b
AAZWD <azumrawi@gmail.com>
2021-03-30 18:43:27 -0700

    3/30

--- public/user/js/script.js
+++ public/user/js/script.js
@@ -11,31 +11,42 @@ window.onload = function () {
                 if (!name.textContent.includes(input.value))
                     row.classList.add('hide');
             });
-        }else{
+        } else {
             tRow.forEach(row => {
-                    row.classList.remove('hide');
+                row.classList.remove('hide');
             });
         }
-        
+
     }
 
 
-       //filter by id
-       let input2 = document.getElementById('idSearch');
-       let tRow2 = document.querySelectorAll('tbody .info');
-       input2.onkeyup = function (event) {
-           if (input2.value) {
-               tRow2.forEach(row => {
-                   let id = row.querySelector('.id');
-                   if (!id.textContent.includes(input2.value))
-                       row.classList.add('hide');
-               });
-           }else{
-               tRow2.forEach(row => {
-                       row.classList.remove('hide');
-               });
-           }
-           
-       }
-    
+    //filter by id
+    let input2 = document.getElementById('idSearch');
+    let tRow2 = document.querySelectorAll('tbody .info');
+    input2.onkeyup = function (event) {
+        if (input2.value) {
+            tRow2.forEach(row => {
+                let id = row.querySelector('.id');
+                if (!id.textContent.includes(input2.value))
+                    row.classList.add('hide');
+            });
+        } else {
+            tRow2.forEach(row => {
+                row.classList.remove('hide');
+            });
+        }
+
+    };
+
+    ///delete patient confirmation
+    let form = document.querySelector('.deleteForm');
+    let del = document.querySelector('.deleteBtn');
+    form.onsubmit = function(event) {
+        let pID = del.value;
+        if(confirm(`Delete Patient # ${pID} ?`))
+            return true
+        else
+            event.preventDefault();
+    }
+
 };
--- server.js
+++ server.js
@@ -192,20 +192,25 @@ app.get('/user/edit_profile', (req, res) => {
         date = date.toDateString();
         let fname = 'New'
         let lname = 'User'
-
         if (uData[0].fname) fname = uData[0].fname
         if (uData[0].lname) lname = uData[0].lname
-        //get patient info from db
-        //get test info from db
-        //pass both to page
-        res.render('user/edit_profile',
-            {
-                fname: fname,
-                lname: lname,
-                date: date,
-                page: 'Edit Profiles'
-            }
-        );
+        console.log('uData', uData);
+        //get patient data
+        patientCollection.find({uID: uData[0]._id}, function (err, pResult) {
+            console.log('find patients result', pResult)
+
+            //get test data with patient _id
+            res.render('user/edit_profile',
+                {
+                    fname: fname,
+                    lname: lname,
+                    date: date,
+                    page: 'Edit Profiles',
+                    pData: pResult
+                    //tData: tResult
+                }
+            );
+        });
     } else {
         res.redirect('/error');
     }
